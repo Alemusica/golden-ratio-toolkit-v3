e3757daf373b06e18e3f8113ea660be951d01581
alelab <alessio.cazzaniga87@gmail.com>
2025-04-28 08:41:58 +0200

    edits

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export * from "./lib/golden-ratio";
-export * from "./lib/react";
-export { createTailwindPlugin } from "./lib/tailwind";
+export * from './lib/golden-ratio';
 export * from './lib/color';
-export * from './lib/color';
-export * from './lib/layout';
+export * from './lib/layout';      // <-- dopo aver sistemato layout.ts
+export { createTailwindPlugin } from './lib/tailwind';
